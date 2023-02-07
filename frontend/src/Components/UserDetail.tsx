bebad0937bf2fd5c8a6f8606d12dcce521a78b37
Bindiya <bindiya.kejdiwal@gmail.com>
2023-02-07 12:20:53 +0000

    Added scss for UserDeatil page

--- frontend/src/Components/UserDetail.tsx
+++ frontend/src/Components/UserDetail.tsx
@@ -4,41 +4,44 @@ import { useState, useEffect } from "react";
 import { Page } from "../../../src/models/api/page";
 import { UserModel } from "../../../src/models/api/userModel"
 
+import '../style/UserDetail.scss';
+
+
 export function UserDetail(props: {
     userDetailID: number
 }) {
     const [myData, setMyData] = useState<UserModel | null>(null);
 
 
-    useEffect(() => { fetch(`http://localhost:3001/users/${props.userDetailID}`)
-        .then(response => response.json())
-        .then(data => setMyData(data)) }, [props.userDetailID]);
+    useEffect(() => {
+        fetch(`http://localhost:3001/users/${props.userDetailID}`)
+            .then(response => response.json())
+            .then(data => setMyData(data))
+    }, [props.userDetailID]);
 
 
     let userPostList = [];
     if (myData) {
-        for (let i = 0; i < myData.posts.length; i++) {
-            userPostList.push(<div>
+        for (let i = 0; i < 5; i++) {
+            userPostList.push(<div className="userpostindividual">
                 <p>{myData.posts[i].message}</p>
-                <p>{ myData.posts[i].createdAt.toString()}</p>
+                <p>{myData.posts[i].createdAt.toString()}</p>
                 <p><img src={myData.posts[i].imageUrl}></img></p>
             </div>);
         }
     }
 
 
-    // results	
-    // coverImageUrl	"https://picsum.photos/id/302/2100/800"
-
     if (!myData) { return <div>Waiting for data!</div> }
 
-    return (<div>
+    return (<div className="userprofile">
         <h2>Users</h2>
-        <img src={myData.profileImageUrl}></img>
+        <img id="coverimg" src={myData.coverImageUrl}></img>
+        <img id="profileimg" src={myData.profileImageUrl}></img>
         <p>{myData.name}</p>
         <p>{myData.username}</p>
-        <p>{ myData.email}</p>
-        < div className="user-container">
+        <p>{myData.email}</p>
+        < div className="userpostcontainer">
             {userPostList}
         </div>
     </div>
